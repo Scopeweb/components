292c91cdc41d02e649f40f05eceec83a1094418b
Stephan Moerman <stephan@scopeweb.nyc>
2018-11-27 03:53:15 +0100

    Added client side GA to all pages

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import ReactGA from 'react-ga';
 
 import Jumbotron from './Jumbotron/Jumbotron';
 import NavbarDark from '../Global/NavbarDark/NavbarDark';
@@ -7,7 +8,22 @@ import DottedGrid from './DottedGrid/DottedGrid';
 import OurValues from './OurValues/OurValues';
 import Footer from '../Global/Footer/Footer';
 
+export const initGA= () => {
+    console.log('GAinit')
+    ReactGA.initialize('UA-129909734-1')
+}
+export const logPageView = () => {
+    ReactGA.set({ page: window.location.pathname })
+    ReactGA.pageview(window.location.pathname)
+}
+
 class About extends Component {
+
+    componentDidMount () {
+        initGA()
+        logPageView()
+    }
+
     render() {
         return (
             <React.Fragment>
--- src/Components/DesignDevelopment/DesignDevelopment.js
+++ src/Components/DesignDevelopment/DesignDevelopment.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import ReactGA from 'react-ga';
 
 import Jumbotron from './Jumbotron/Jumbotron';
 import NavbarDark from '../Global/NavbarDark/NavbarDark';
@@ -7,10 +8,24 @@ import Heading from './Heading/Heading';
 import AnalyzeDesignDevelop from './TopCards/TopCards';
 import ProcessHeading from './ProcessHeading/ProcessHeading';
 import Steps from './Steps/Steps';
-
 import Footer from '../Global/Footer/Footer';
 
+export const initGA= () => {
+    console.log('GAinit')
+    ReactGA.initialize('UA-129909734-1')
+}
+export const logPageView = () => {
+    ReactGA.set({ page: window.location.pathname })
+    ReactGA.pageview(window.location.pathname)
+}
+
 class About extends Component {
+
+    componentDidMount () {
+        initGA()
+        logPageView()
+    }
+
     render() {
         return (
             <React.Fragment>
