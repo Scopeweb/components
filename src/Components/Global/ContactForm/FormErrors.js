53c4b785f4247186c575615c88ce8c485f362148
Stephan Moerman <stephan@scopeweb.nyc>
2018-12-06 17:40:56 +0100

    Adding sendgrid contact form

---
+++ src/Components/Global/ContactForm/FormErrors.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const ERROR_SPACING = 10;
+let ErrorTypeColor = 'red';
+let fontSize = '18px';
+
+export default class FormErrors extends Component {
+  render() {
+    if (this.props.errorType && this.props.errorType.match(/Success.*/)) {
+      ErrorTypeColor = 'green';
+    }
+    return (
+      <div style={{fontSize: fontSize, marginTop: ERROR_SPACING + 'px', color: ErrorTypeColor}}>
+        {this.props.errorType}
+      </div>
+    )
+  }
+}
