630b6103ded8a688dde85e9cedfda73667a70afc
Stephan Moerman <stephan@scopeweb.nyc>
2019-01-21 12:06:09 +0100

    Updates

---
+++ precache-manifest.9c362ef0ebf5fdb8aa98ce56200cc269.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "61e98a9b8d9c234c08f6",
+    "url": "/static/css/main.545948c3.chunk.css"
+  },
+  {
+    "revision": "61e98a9b8d9c234c08f6",
+    "url": "/static/js/main.61e98a9b.chunk.js"
+  },
+  {
+    "revision": "e5e5e43d6a4189254db9",
+    "url": "/static/js/1.e5e5e43d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3dc4d3cb6df3d8d50f98910550084c43",
+    "url": "/static/media/bayron-case-img-01.3dc4d3cb.png"
+  },
+  {
+    "revision": "f4904aca72bd0d98110e10a8cd6b9851",
+    "url": "/static/media/bayron-case-img-02.f4904aca.png"
+  },
+  {
+    "revision": "7b66c31ffcd435da734cea2ab4bf7d97",
+    "url": "/static/media/bayron-case-img-03.7b66c31f.png"
+  },
+  {
+    "revision": "e02a5e432ec996e6f0c1a8c83cf14391",
+    "url": "/static/media/bayron-case-img-04.e02a5e43.png"
+  },
+  {
+    "revision": "0405cd6c19efc1af8b7260aa8471e446",
+    "url": "/static/media/bayron-case-img-01.0405cd6c.webp"
+  },
+  {
+    "revision": "75b2e6474b4e74630544e8a94e99f534",
+    "url": "/static/media/bayron-case-img-02.75b2e647.webp"
+  },
+  {
+    "revision": "e5a34b1fb733bae8476d44593b126b1c",
+    "url": "/static/media/bayron-case-img-03.e5a34b1f.webp"
+  },
+  {
+    "revision": "e75e74b519ea764560aa868ecec4a8ba",
+    "url": "/static/media/bayron-case-img-04.e75e74b5.webp"
+  },
+  {
+    "revision": "27f44c52a655c74be35c2857e6c6e60d",
+    "url": "/static/media/dotted-grid-411.27f44c52.png"
+  },
+  {
+    "revision": "a7f3d58bb670e3a65c1b2f911df0e67d",
+    "url": "/static/media/logo-light.a7f3d58b.png"
+  },
+  {
+    "revision": "c92acd301541c7970dc65e730d889cf7",
+    "url": "/static/media/logo-light.c92acd30.webp"
+  },
+  {
+    "revision": "72875facbf33e36ef5645f2b1a2855a3",
+    "url": "/static/media/bayron-home-improvements_case_image.72875fac.png"
+  },
+  {
+    "revision": "d04860a708bd08978a39468c707a63fc",
+    "url": "/static/media/bayron-home-improvements_case_image.d04860a7.webp"
+  },
+  {
+    "revision": "7c5b467f3ad21936fd1cfb1a754c6d38",
+    "url": "/static/media/go-remote_case_image.7c5b467f.png"
+  },
+  {
+    "revision": "ae57b932bfd3e93a22a4560deffe1c44",
+    "url": "/static/media/go-remote_case_image.ae57b932.webp"
+  },
+  {
+    "revision": "a1dbba480799576406e0b19e75678f6c",
+    "url": "/static/media/gemini-gas-springs_case_image.a1dbba48.png"
+  },
+  {
+    "revision": "41672a11af9f9a1f80a701fd8824de94",
+    "url": "/static/media/gemini-gas-springs_case_image.41672a11.webp"
+  },
+  {
+    "revision": "3ed5ac75b7ce7959eb41dede97864d8d",
+    "url": "/static/media/giveasy_case_image.3ed5ac75.png"
+  },
+  {
+    "revision": "d85446b146005e5801ef7028541eaa48",
+    "url": "/static/media/giveasy_case_image.d85446b1.webp"
+  },
+  {
+    "revision": "34dd7b73e4c53d0229e301fd4c973f79",
+    "url": "/static/media/dotted-grid-320.34dd7b73.png"
+  },
+  {
+    "revision": "d948112f1e0ac22a62745329f4da744a",
+    "url": "/static/media/dotted-grid-768.d948112f.png"
+  },
+  {
+    "revision": "c75bfd3fe5765e46c88e812b3321c931",
+    "url": "/static/media/dotted-grid-1280.c75bfd3f.png"
+  },
+  {
+    "revision": "0285510e9c3bf03f5e5b3497e4cab5cd",
+    "url": "/static/media/tradegothic.0285510e.woff"
+  },
+  {
+    "revision": "326b25561a81a75783698e64e86699dc",
+    "url": "/static/media/tradegothic-light.326b2556.woff"
+  },
+  {
+    "revision": "4a88d642287f291790df4a0ecc8ed715",
+    "url": "/static/media/tradegothic-bold.4a88d642.woff"
+  },
+  {
+    "revision": "653cf718628ed44fde5cedd82fd87e17",
+    "url": "/static/media/AvantGarde-Book.653cf718.woff"
+  },
+  {
+    "revision": "4709062d4f33ca508d32fd1ff42042a3",
+    "url": "/static/media/AvantGarde-Light.4709062d.woff"
+  },
+  {
+    "revision": "87b71d349f52fbfc015f95f36e5d6d3e",
+    "url": "/static/media/AvantGarde-Bold.87b71d34.woff"
+  },
+  {
+    "revision": "b5abc4fdfc0b7aa47371a291ab638d0b",
+    "url": "/static/media/Pixeicons.b5abc4fd.eot"
+  },
+  {
+    "revision": "ccdcc4a25347956348f8e7d55e82b612",
+    "url": "/static/media/Pixeicons.ccdcc4a2.ttf"
+  },
+  {
+    "revision": "afb36614e4d9a46dad76d13363c6bd02",
+    "url": "/static/media/Pixeicons.afb36614.woff"
+  },
+  {
+    "revision": "051e2ed483dab0f278ba8d8cd66067fc",
+    "url": "/static/media/Pixeicons.051e2ed4.svg"
+  },
+  {
+    "revision": "4b55b33569194116c222a9d2721d2877",
+    "url": "/static/media/Pixeicons.4b55b335.eot"
+  },
+  {
+    "revision": "c951a34533fe9a9f1a1ecd39557e8a70",
+    "url": "/static/media/Pixeicons.c951a345.ttf"
+  },
+  {
+    "revision": "4f604a02cee802b0186513d4a570206e",
+    "url": "/static/media/Pixeicons.4f604a02.woff"
+  },
+  {
+    "revision": "a4d8847b2c55a44ff487921eabf77fb5",
+    "url": "/static/media/Pixeicons.a4d8847b.eot"
+  },
+  {
+    "revision": "e7adf6cd68fd820a59eb519543bd0636",
+    "url": "/static/media/Pixeicons.e7adf6cd.ttf"
+  },
+  {
+    "revision": "08de4364e4e4ceabea2c6302853c0a70",
+    "url": "/static/media/Pixeicons.08de4364.woff"
+  },
+  {
+    "revision": "ff99a6d3cebbc5fbeedfc507a019584e",
+    "url": "/static/media/Pixeicons.ff99a6d3.svg"
+  },
+  {
+    "revision": "a03b2e1e7ae0deba38fa0395df6d96ea",
+    "url": "/static/media/Pixeicons.a03b2e1e.svg"
+  },
+  {
+    "revision": "d21a669162e243c013b5965a16ab247c",
+    "url": "/static/media/logo.d21a6691.png"
+  },
+  {
+    "revision": "e1dad291270689aba8c874202bb5bb9e",
+    "url": "/index.html"
+  }
+];
